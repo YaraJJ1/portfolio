356db7d454f8dc82863158148db013b7d610edaa
YaraJJ1 <janahi.yara@gmail.com>
2026-09-02 05:21:52 +0300

    round 6

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { db } from "./firebase.js";
+
 import {
     getFirestore
-} 
-from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDepBOMROJ5MrmUYAYpm5dcbWXME8ZB5Fg",
@@ -13,7 +13,8 @@ const firebaseConfig = {
     messagingSenderId: "723210965924",
     appId: "1:723210965924:web:0813dfb9f9a537bf2be731",
     measurementId: "G-TWZTQX4HHG"
-}
+};
+
 
 const app = initializeApp(firebaseConfig);
 
